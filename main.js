52bd93aced520d1cf4a392f3d939d612b82161f5
OG'ABEK <shomoyxhacker007@mail.ru>
2023-03-09 21:37:47 +0500

    Typescript intro

---
+++ main.js
@@ -0,0 +1,6 @@
+var test = "Hello";
+function getTime() {
+    return new Date().getTime();
+}
+console.log(getTime());
+console.log(test);
---
+++ main.ts
@@ -0,0 +1,8 @@
+const test = "Hello";
+
+function getTime(): number {
+  return new Date().getTime();
+}
+
+console.log(getTime());
+console.log(test);
